e3eb0b61c45696498a42671934e3801c3575770c
Ali-roozbahani-dev <ali.roozbahani1379@gmail.com>
2026-09-20 21:43:48 +0330

    feat : Implementing infinite scroll on favorites list

--- src/components/Features/Favorites/favorites_list/hooks/useFavorites.ts
+++ src/components/Features/Favorites/favorites_list/hooks/useFavorites.ts
@@ -1,10 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { favoriteQueryKeys } from "../../favoriteQueryKeys";
 import { getFavorites } from "../api/getFavorites";
 
 export function useFavorites() {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: favoriteQueryKeys.list(),
-    queryFn: getFavorites,
+
+    queryFn: ({pageParam})=> getFavorites(pageParam),
+
+    initialPageParam: 1,
+
+    getNextPageParam : (lastPage)=>{
+      if(!lastPage.next) return undefined;
+
+      const url = new URL(lastPage.next);
+      return Number(url.searchParams.get("page"));
+    } 
   });
 }
